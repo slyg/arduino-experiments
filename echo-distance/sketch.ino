6d8372ef3bb87879531902520cf404147bf875cd
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-08-13 19:47:14 +0100

    Add sonic sensor

---
+++ echo-distance/sketch.ino
@@ -0,0 +1,36 @@
+#define PWM_TRIG 9
+#define PWM_ECHO 10
+
+long duration;
+int distance;
+
+void setup() {
+
+  pinMode(PWM_TRIG, OUTPUT);
+  pinMode(PWM_ECHO, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // reset trigger
+  digitalWrite(PWM_TRIG, LOW);
+  delayMicroseconds(10);
+
+  // trigger for 10μs,
+  // signaling to the sensor to do a 10μs sonic bursts
+  digitalWrite(PWM_TRIG, HIGH);
+  delayMicroseconds(10); // the sensor generates sonic bursts during this time
+  digitalWrite(PWM_TRIG, LOW);
+
+  // Compute duration
+  // Read the time when the echo comes back
+  duration = pulseIn(PWM_ECHO, HIGH); // in μs
+  distance = duration*0.034/2; // in cm
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" cm\n");
+
+}
