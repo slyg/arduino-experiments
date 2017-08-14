aff1694aab43a07b85f1d7cfe0f84b387b00d680
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-08-14 23:03:56 +0100

    Add mock server

---
+++ radar/socketserver/mock.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 9999 })
+
+function* gen() {
+  let angle = 0
+  while (angle <= 180) {
+    yield { angle, distance: Math.floor(Math.random() * 30) }
+    angle++
+  }
+  yield* gen()
+}
+
+wss.on('connection', function connection(ws) {
+
+  const data = gen()
+
+  setInterval(() => {
+    ws.send(JSON.stringify(data.next().value))
+  }, 20)
+
+})
