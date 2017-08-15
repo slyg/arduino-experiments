5822177c2796993c9af80722a31c4501b362a90e
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-08-15 14:45:58 +0100

    Make mock data a bit more realistic

--- radar/socketserver/mock.js
+++ radar/socketserver/mock.js
@@ -2,14 +2,37 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: 9999 })
 
+const pseudoObstacle = angle => {
+  if (angle > 45 && angle < 90) {
+    return 20
+  } else if (angle > 110 && angle < 150) {
+    return 30
+  } else {
+    return 45
+  }
+}
+
+const distanceRandomizer = angle =>
+  Math.floor(Math.random() * 2) + pseudoObstacle(angle)
+
 function* gen() {
   let angle = 0
   while (angle <= 180) {
-    yield { angle, distance: Math.floor(Math.random() * 30) }
+    yield { angle, distance: distanceRandomizer(angle) }
     angle++
+  }
+  yield* invGen()
+}
+
+function* invGen() {
+  let angle = 180
+  while (angle >= 0) {
+    yield { angle, distance: distanceRandomizer(angle) }
+    angle--
   }
   yield* gen()
 }
+
 
 wss.on('connection', function connection(ws) {
 
@@ -17,7 +40,7 @@ wss.on('connection', function connection(ws) {
 
   const id = setInterval(() => {
     ws.send(JSON.stringify(data.next().value))
-  }, 20)
+  }, 10)
 
   ws.on('close', () => {
     clearInterval(id)
